16ceef625b08f862656ed2d435afa1128476feed
Milos Zivkovic <milos.zivkovic@livenation.com>
2018-09-19 12:26:53 +0200

    added separator

--- src/main/java/com/ef/config/cli/FileCliArguments.java
+++ src/main/java/com/ef/config/cli/FileCliArguments.java
@@ -1,12 +1,16 @@
 package com.ef.config.cli;
 
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import lombok.Data;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 
+@Data
+@Parameters(separators = "=")
 public class FileCliArguments {
     @Parameter(names = "--accesslog", description = "Path to the access log file to be proccessed")
     private String filePath;
